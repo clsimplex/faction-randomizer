437d16a7f6b75cdf0dce8417b762ac43ded4609f
LeebranZadwell <levon@clsimplex.com>
2016-04-26 18:39:48 -0700

    update

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# house-randomizer
+# house_randomizer
 
 This is used with the 'Game of Thrones' board game.
 
-## As is
-
-This will forever be a prototype and things like 'what if you enter 7 players?' will not be worried about.
-
-## PR
+## AS IS and PRs
 
 We may respond, we may not. Enjoy our small thank you to the open source community.
+
+We offer no promises nor documentation.
+
+## Disclaimer
+
+Fair use of copyright and such. We obviously aren't affiliated in any way to any of the Game of Thrones people.
---
+++ house_randomizer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# CL Simplex 2016
+# Levon Zadravec-Powell
+# Eric Moolin
+# Free Beer License
+# As is - no warranty. Go away.
+
+# This is a companion for the second edition of the Game of Thrones board game.
+# This is the part where we disclaim any affliation with their copyright and stuff.
+
+import sys
+from random import shuffle
+
+# Order matters.
+HOUSES = ['STARK', 'LANNISTER', 'BARATHEON', 'GREYJOY', 'TRYELL', 'MARTELL']
+
+def randomize_houses(names):
+	"""
+	Given a list of names, assign houses to them and return them.
+
+	Returns a dictionary of the form: { PLAYER: HOUSE }
+	"""
+	number_of_players = len(names)
+
+	if number_of_players < 3 or number_of_players > 6:
+		raise Exception('Incorrect number of players. You need 3-6.')
+
+	# This enforces which houses are available based on the number of players.
+	houses_in_use = HOUSES[:number_of_players]
+
+	shuffle(houses_in_use)
+
+	# Generates dictionary using Python list comprehension
+	return { player:houses_in_use.pop() for player in names }
+
+# Main support.
+
+if __name__ == '__main__':
+	print( randomize_houses(sys.argv[1:]) )
