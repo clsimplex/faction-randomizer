2d1328beb85b5c7f2156a97ee07e02ec95ef9a87
Levon Zadravec-Powell <levon@clsimplex.com>
2019-04-30 10:08:00 -0700

    Document - additional documentation

--- game_of_thrones.py
+++ game_of_thrones.py
@@ -3,19 +3,19 @@
 import sys
 from random import shuffle
 # CL Simplex 2016
-# Last Updated: April 17 2019
+# Last Updated: April 30 2019
 # Levon Zadravec-Powell
 # Eric Moolin
 # Free Beer License
 # As is - no warranty. Go away.
 
-# This is a companion for the second edition of the Game of Thrones board game.
+# This is a companion for the second edition of the Game of Thrones board game. Mother of Dragons compatible.
 # This is the part where we disclaim any affliation with their copyright and stuff.
 
 
 HOUSES = ['STARK', 'LANNISTER', 'BARATHEON', 'ARRYN', 'GREYJOY', 'TRYELL', 'MARTELL']
 
-def randomize(names):
+def assign_houses(names):
   """
 	Given a list of names assign GOT houses to them.
 
@@ -26,7 +26,7 @@ def randomize(names):
   number_of_players = len(names)
 
   if number_of_players < 3 or number_of_players > 8:
-    raise Exception('Incorrect number of players. You need 3-8.')
+    raise Exception('A man needs 3-8 names.')
 
   houses_in_use = HOUSES
 
@@ -39,6 +39,5 @@ def randomize(names):
   # Generates dictionary using Python list comprehension
   return { player:houses_in_use.pop() for player in names }
 
-# Main support.
 if __name__ == '__main__':
-  print(randomize(sys.argv[1:]))
+  print(assign_houses(sys.argv[1:]))
